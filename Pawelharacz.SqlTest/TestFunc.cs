4569b531cb2b0aaf4eea4833c26010a1b86e3b45
Paweł Haracz <haracz5@live.com>
2020-01-12 14:43:02 +0100

    Inner Join and Dynamic Object  #3 (#6)
    * support add dynamic object
    
    * uncomment nuget push

--- Pawelharacz.SqlTest/TestFunc.cs
+++ Pawelharacz.SqlTest/TestFunc.cs
@@ -26,9 +26,21 @@ namespace Pawelharacz.SqlTest
         
         [FunctionName("TestFunc1")]
         public static async Task<IActionResult> RunFilterAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "TestFunc/{id}")]
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "TestFunc/{id:int}")]
             HttpRequest req, 
             [MsSqlDb(SqlQuery = "Select Id, Name from Examples where id = {id}", ConnectionStringSetting = "MsSqlConnectionString")] IEnumerable<Example> examples,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return  new OkObjectResult(JsonConvert.SerializeObject(examples));
+        }
+        
+        [FunctionName("TestFunc3")]
+        public static async Task<IActionResult> RunSQLInjection(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "TestFunc/{name}")]
+            HttpRequest req, 
+            [MsSqlDb(SqlQuery = "Select Id, Name from Examples where Name = {name}", ConnectionStringSetting = "MsSqlConnectionString")] IEnumerable<Example> examples,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -47,11 +59,42 @@ namespace Pawelharacz.SqlTest
 
             return  new OkObjectResult(JsonConvert.SerializeObject(example));
         }
+
+        [FunctionName("Join")]
+        public static async Task<IActionResult> Join(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, 
+            [MsSqlDb(SqlQuery = "SELECT e.Id as Id, e.Name as Name, e.RelationshipId as RelationshipId, c.Name as RelationshipName FROM [dbo].[Examples] e Inner join [dbo].[Relationships] c on c.Id =e.RelationshipId", ConnectionStringSetting = "MsSqlConnectionString")]IEnumerable<JoinExample> examples,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return  new OkObjectResult(JsonConvert.SerializeObject(examples));
+            
+        }
+        
+        [FunctionName("Join1")]
+        public static async Task<IActionResult> Joi1(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, 
+            [MsSqlDb(SqlQuery = "SELECT e.Id as Id, e.Name as Name, e.RelationshipId as RelationshipId, c.Name as RelationshipName FROM [dbo].[Examples] e Inner join [dbo].[Relationships] c on c.Id =e.RelationshipId", ConnectionStringSetting = "MsSqlConnectionString")]IEnumerable<dynamic> examples,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return  new OkObjectResult(JsonConvert.SerializeObject(examples));
+            
+        }
     }
 
     public class Example
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public int Id { get; set; }
+    }
+    public class JoinExample : Example
+    {
+        public string RelationshipName { get; set; }
+        public int RelationshipId { get; set; }
     }
 }
--- Pawelharacz.Webjobs.Extensions.MSSqlDatabase/Service/IMsSqlDbService.cs
+++ Pawelharacz.Webjobs.Extensions.MSSqlDatabase/Service/IMsSqlDbService.cs
@@ -11,8 +11,12 @@ namespace Pawelharacz.Webjobs.Extensions.MSSqlDatabase.Service
         #if NETCOREAPP3_1
         IAsyncEnumerable<T> GetAsync<T>(MsSqlSpec msSqlSpec,
             [EnumeratorCancellation] CancellationToken cancellationToken = default) where T : class, new();
+
+        IAsyncEnumerable<object> GetAsync(MsSqlSpec msSqlSpec,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default); 
         #else
             Task<IEnumerable<T>> GetAsync<T>(MsSqlSpec msSqlSpec, CancellationToken cancellationToken = default) where T : class, new();
+            Task<IEnumerable<object>> GetAsync(MsSqlSpec msSqlSpec, CancellationToken cancellationToken = default);
         #endif
         
         Task<T> GetOne<T>(MsSqlSpec msSqlSpec,CancellationToken cancellationToken = default) where T : class, new();
